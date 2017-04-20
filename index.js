8ac5e0964ed285a6d609dc475d896acbe500af4b
Maxwel Dsouza <maxellusionist@gmail.com>
2017-04-20 17:54:55 +0530

    Use aftership eslint config

--- index.js
+++ index.js
@@ -5,43 +5,44 @@ const ZERO_DELAY = 0;
 const TIME_TO_RUN = 10;
 
 export class Connection {
-    constructor (client) {
-        this.client = client;
-    }
-    send () {
-        return new Promise((resolve, reject) => {
-            let payload = { 'hello' : 'world' };
-            this.client.put(DEFAULT_PRIORITY, ZERO_DELAY, TIME_TO_RUN, JSON.stringify(payload), (err, jobid) => {
-                if (err) {
-                    reject();
-                } else {
-                    console.log(`Job added ${jobid}`)
-                    resolve(this);
-                }
-            })
-        })
-    }
+	constructor(client) {
+		this.client = client;
+	}
+	send() {
+		return new Promise((resolve, reject) => {
+			let payload = {hello: 'world'};
+			this.client.put(DEFAULT_PRIORITY, ZERO_DELAY, TIME_TO_RUN, JSON.stringify(payload), (err, jobid) => {
+				if (err) {
+					reject();
+				} else {
+					console.log(`Job added ${jobid}`);
+					resolve(this);
+				}
+			});
+		});
+	}
 }
 
 export class Producer {
-    constructor ({ hostname, port }) {
-        this.client = new fivebeans.client(hostname, port);
-    }
-    connect () {
-        return new Promise((resolve, reject) => {
-            this.client.on('connect', () => {
-                let connection = new Connection(this.client);
-                resolve (connection);
-                console.log('Connected to beanstalkd');
-            })
-            .on('error', (err) => {
-                reject ('failed');
-                console.error('Couldnt connect to beanstalkd', err);
-            })
-            .on('close', () => {
-                console.log('Beanstalkd connection closed');
-            })
-            .connect();
-        });
-    }
+	constructor({hostname, port}) {
+		this.client = new fivebeans.client(hostname, port);
+	}
+	connect() {
+		return new Promise((resolve, reject) => {
+			this.client
+				.on('connect', () => {
+					let connection = new Connection(this.client);
+					resolve(connection);
+					console.log('Connected to beanstalkd');
+				})
+				.on('error', err => {
+					reject('failed');
+					console.error('Couldnt connect to beanstalkd', err);
+				})
+				.on('close', () => {
+					console.log('Beanstalkd connection closed');
+				})
+				.connect();
+		});
+	}
 }
