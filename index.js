a1cf9e63453769707cc8867806f568c2077bf85d
Maxwel Dsouza <maxellusionist@gmail.com>
2017-04-20 22:18:20 +0530

    Derive from base class

--- index.js
+++ index.js
@@ -4,11 +4,49 @@ const DEFAULT_PRIORITY = 1;
 const ZERO_DELAY = 0;
 const TIME_TO_RUN = 10;
 
-export class Producer {
+class Base {
     constructor({hostname, port, tube}) {
         this.client = new fivebeans.client(hostname, port);
         this.tube = tube;
     }
+    _tubename() {
+        return new Promise((resolve, reject) => {
+            this.client.list_tube_used((err, tubename) => {
+                if (err) {
+                    reject(err);
+                }
+                resolve(tubename);
+            });
+        });
+    }
+    _delete_all_ready(callback) {
+        this.client.peek_ready((err, jobid) => {
+            if (err === 'NOT_FOUND') {
+                callback();
+                console.log('All ready jobs deleted');
+            } else if (err) {
+                console.error('Could not peek ready jobs', err);
+            } else {
+                this.client.destroy(jobid, error => {
+                    if (error) {
+                        console.error('Could not delete job', err);
+                    } else {
+                        this._delete_all_ready(callback);
+                    }
+                });
+            }
+        });
+    }
+	quit() {
+		return new Promise((resolve, reject) => {
+			this.client.quit(() => {
+				resolve();
+			});
+		});
+	}
+}
+
+export class Producer extends Base {
     connect() {
         return new Promise((resolve, reject) => {
             this.client
@@ -28,34 +66,6 @@ export class Producer {
                 .connect();
         });
     }
-	_tubename() {
-		return new Promise((resolve, reject) => {
-			this.client.list_tube_used((err, tubename) => {
-				if (err) {
-					reject(err);
-				}
-				resolve(tubename);
-			});
-		});
-	}
-	_delete_all_ready(callback) {
-		this.client.peek_ready((err, jobid) => {
-			if (err === 'NOT_FOUND') {
-				callback();
-				console.log('All ready jobs deleted');
-			} else if (err) {
-				console.error('Could not peek ready jobs', err);
-			} else {
-				this.client.destroy(jobid, error => {
-					if (error) {
-						console.error('Could not delete job', err);
-					} else {
-						this._delete_all_ready(callback);
-					}
-				});
-			}
-		});
-	}
 	send() {
 		return new Promise((resolve, reject) => {
 			let payload = {hello: 'world'};
@@ -80,11 +90,4 @@ export class Producer {
             })
         });
     }
-	quit() {
-		return new Promise((resolve, reject) => {
-			this.client.quit(() => {
-				resolve();
-			});
-		});
-	}
 }
