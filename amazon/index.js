429150fd920ba56808b0fdfcde073fbe61b3cd2b
Maxwel Dsouza <maxellusionist@gmail.com>
2017-04-25 20:59:35 +0530

    Fix amazon credentials

--- amazon/index.js
+++ amazon/index.js
@@ -1,4 +1,8 @@
 import * as AWS from 'aws-sdk';
+import * as config from '../config.json';
+
+let awsconfig = new AWS.Config();
+AWS.config.update(config.amazon);
 let ses = new AWS.SES({apiVersion: '2010-12-01'});
 
 export function prepare({from, to, text, subject}) {
@@ -34,12 +38,12 @@ export default {
 	async send(mail) {
 		let params = prepare(mail);
 		console.log('Simulated amazon mail');
-		// ses.sendEmail(params, function (err, data) {
-		// 	if (err) {
-		// 		console.log(err, err.stack);
-		// 	} else {
-		// 		console.log(data);
-		// 	}
-		// });
+		ses.sendEmail(params, function (err, data) {
+			if (err) {
+				console.log(err, err.stack);
+			} else {
+				console.log(data);
+			}
+		});
 	}
 };
