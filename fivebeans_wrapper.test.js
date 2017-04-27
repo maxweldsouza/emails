472f87b5b8900333e18b7fd19bdfce58bcb8adfb
Maxwel Dsouza <maxellusionist@gmail.com>
2017-04-27 20:10:44 +0530

    Cleanup fivebeans wrapper tests

--- fivebeans_wrapper.test.js
+++ fivebeans_wrapper.test.js
@@ -1,75 +1,72 @@
 import FiveBeans from './fivebeans_wrapper';
-import * as config from './config.json';
-
-let {hostname, port, tube} = config.beanstalkd;
 
 test('Can use async await', async () => {
-	let fb = new FiveBeans();
-	await fb.connect();
-	await fb.quit();
+	let fivebeans = new FiveBeans();
+	await fivebeans.connect();
+	await fivebeans.quit();
 });
 
 test('Can use promises', () => {
-	let fb = new FiveBeans();
-	return fb.connect().then(() => {
-		return fb.quit();
+	let fivebeans = new FiveBeans();
+	return fivebeans.connect().then(() => {
+		return fivebeans.quit();
 	});
 });
 
 test('Clear tube deletes all ready jobs', async () => {
-	let fb = new FiveBeans();
+	let fivebeans = new FiveBeans();
 	try {
-		await fb.connect();
-		await fb.put({priority: 0, delay: 0, payload: {hello: 'world'}});
-		await fb.put({priority: 0, delay: 0, payload: {hello: 'world'}});
-		await fb.put({priority: 1, delay: 0, payload: {hello: 'world'}});
-		await fb.put({priority: 1, delay: 0, payload: {hello: 'world'}});
-		await fb._danger_clear_tube();
-		await fb.peek_ready();
-		await fb.quit();
+		await fivebeans.connect();
+		await fivebeans.put({priority: 0, delay: 0, payload: {hello: 'world'}});
+		await fivebeans.put({priority: 0, delay: 0, payload: {hello: 'world'}});
+		await fivebeans.put({priority: 1, delay: 0, payload: {hello: 'world'}});
+		await fivebeans.put({priority: 1, delay: 0, payload: {hello: 'world'}});
+		await fivebeans._danger_clear_tube();
+		await fivebeans.peek_ready();
+		await fivebeans.quit();
 	} catch (e) {
 		expect(e).toEqual('NOT_FOUND');
 	}
 });
 
 describe('Beanstalkd tests', () => {
-	let fb;
+	let fivebeans;
 	beforeEach(async () => {
-		await fb._danger_clear_tube();
+		await fivebeans._danger_clear_tube();
 	});
 
 	beforeAll(async () => {
-		fb = new FiveBeans();
-		await fb.connect();
+		fivebeans = new FiveBeans();
+		await fivebeans.connect();
 	});
 
 	test('Can use tube', async () => {
-		let tubename = await fb.use('test_fivebeans_wrapper');
+		let tubename = await fivebeans.use('test_fivebeans_wrapper');
 		expect(tubename).toBe('test_fivebeans_wrapper');
 	});
 
 	test('Can list tube used', async () => {
-		await fb.use('test_fivebeans_wrapper');
-		let tubename = await fb.list_tube_used();
+		await fivebeans.use('test_fivebeans_wrapper');
+		let tubename = await fivebeans.list_tube_used();
 		expect(tubename).toBe('test_fivebeans_wrapper');
 	});
 
 	test('Can watch tube', async () => {
-		await fb.watch('test_fivebeans_wrapper');
+		await fivebeans.watch('test_fivebeans_wrapper');
 	});
 
 	test('Can reserve job', async () => {
-		await fb.put({priority: 0, delay: 0, payload: {hello: 'world'}});
-		let result = await fb.reserve();
+		await fivebeans.put({priority: 0, delay: 0, payload: {hello: 'world'}});
+		let result = await fivebeans.reserve();
 		expect(result.payload).toMatchObject({hello: 'world'});
 	});
 
 	test('Can put job', async () => {
-		let jobid = await fb.put({priority: 0, delay: 0, payload: {hello: 'world'}});
-		await fb.delete(jobid);
+		let jobid = await fivebeans.put({priority: 0, delay: 0, payload: {hello: 'world'}});
+		await fivebeans.delete(jobid);
 	});
 
 	afterAll(async () => {
-		await fb.quit();
+		await fivebeans.quit();
 	});
 });
