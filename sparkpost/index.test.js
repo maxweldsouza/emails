3ae579d14cccf3c6b900e588d1f533121b1236c2
Maxwel Dsouza <maxellusionist@gmail.com>
2017-04-28 00:49:42 +0530

    Sparkpost test

---
+++ sparkpost/index.test.js
@@ -0,0 +1,25 @@
+import sparkpost from './index';
+
+test('Test sparkpost request format', () => {
+	let result =sparkpost.prepare({
+		to: 'something@example.com',
+		from: 'source@example.com',
+		subject: 'Test email',
+		text: 'This is the mail body'
+	});
+	expect(result).toMatchObject({
+		campaign_id: 'postman_inline_both_example',
+		recipients: [
+			{
+				address: 'something@example.com'
+			}
+		],
+		content: {
+			from: {
+				email: 'source@example.com'
+			},
+			subject: 'Test email',
+			text: 'This is the mail body'
+		}
+	});
+});
