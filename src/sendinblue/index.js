d8d86369724b99b1a68ea744f6f183d98b799a7f
Maxwel Dsouza <maxellusionist@gmail.com>
2017-05-01 02:03:32 +0530

    Add sendinblue

---
+++ src/sendinblue/index.js
@@ -0,0 +1,28 @@
+import fetch from 'node-fetch';
+import config from '../config.json';
+import VendorBase from '../vendor_base';
+
+class SendInBlue extends VendorBase {
+	prepare({from, to, text, subject}) {
+		return {
+			from: [from]
+			to: {to: ""}, // Recipient name
+			subject,
+			html: text
+		};
+	}
+	async send(mail) {
+		let body = this.prepare(mail);
+		let res = await fetch(`https://api.sendinblue.com/v2.0/email`, {
+			method: 'POST',
+			headers: {
+				"api-key": config.sendinblue.key
+			},
+			body
+		});
+		let json = await res.json();
+		return json;
+	}
+}
+
+export default new SendInBlue();
